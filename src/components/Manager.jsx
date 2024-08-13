e7ef95cbdfdc66d84db5d22b809409fca117ea34
arpit pandiya <arpitpandiya2003@gmail.com>
2024-08-13 15:52:15 +0530

    store data in local storage and create table

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,28 +1,36 @@
-import React, { useState } from "react";
-import { useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 const Manager = () => {
-  const ref = useRef()
-  const [form, setform] = useState({site: "", username: "", password: ""})
+  const ref = useRef();
+  const [form, setform] = useState({ site: "", username: "", password: "" });
+  const [passwordArray, setPasswordArray] = useState([]);
 
-  const showPassword =() =>{
-      alert("show the password");
-      console.log(ref.current.src);
-      if(ref.current.src.includes("icons/eyecross.png")){
-          ref.current.src = "icons/eye.png"
-      }
-      else{
-        ref.current.src = "icons/eyecross.png"
-      }
-  }
+  useEffect(() => {
+    let passwords = localStorage.getItem("passwords");
+    if (passwords) {
+      setPasswordArray(JSON.parse(passwords));
+    }
+  }, []);
 
-  const savePassword = () =>{
-    console.log(form)
-  }
+  const showPassword = () => {
+    alert("show the password");
+    console.log(ref.current.src);
+    if (ref.current.src.includes("icons/eyecross.png")) {
+      ref.current.src = "icons/eye.png";
+    } else {
+      ref.current.src = "icons/eyecross.png";
+    }
+  };
 
-  const handleChange = (e) =>{
-    setform({...form, [e.target.name]: e.target.value})
-  }
+  const savePassword = () => {
+    setPasswordArray([...passwordArray, form]);
+    localStorage.setItem("passwords", JSON.stringify([...passwordArray, form]));
+    console.log(...passwordArray, form);
+  };
+
+  const handleChange = (e) => {
+    setform({ ...form, [e.target.name]: e.target.value });
+  };
 
   return (
     <>
@@ -59,22 +67,61 @@ const Manager = () => {
               id=""
             />
             <div className="relative">
-            <input
-              value={form.password}
-              onChange={handleChange}
-              placeholder="Enter Password"
-              className="rounded-full border border-green-500 w-full px-4 py-1 text-black" type="text" name="password" id=""/>
-              <span className="absolute right-[3px] top-[4px]  cursor-pointer" onClick={showPassword}>
-              <img ref={ref} className="p-1 w-7 h-6 mt-1 mr-0 pt-0"  src="icons/eye.png" alt="eye"/>
+              <input
+                value={form.password}
+                onChange={handleChange}
+                placeholder="Enter Password"
+                className="rounded-full border border-green-500 w-full px-4 py-1 text-black"
+                type="text"
+                name="password"
+                id=""
+              />
+              <span
+                className="absolute right-[3px] top-[4px]  cursor-pointer"
+                onClick={showPassword}
+              >
+                <img
+                  ref={ref}
+                  className="p-1 w-7 h-6 mt-1 mr-0 pt-0"
+                  src="icons/eye.png"
+                  alt="eye"
+                />
               </span>
-              </div>
+            </div>
           </div>
-          <button onClick={savePassword} className="flex justify-center items-center gap-2 bg-green-400 hover:bg-green-300 rounded-full px-8 py-2 w-fit border border-green-900">
-          <lord-icon
-            src="https://cdn.lordicon.com/jgnvfzqg.json"
-            trigger="hover"
-          ></lord-icon>
-          Add Password</button>
+          <button
+            onClick={savePassword}
+            className="flex justify-center items-center gap-2 bg-green-400 hover:bg-green-300 rounded-full px-8 py-2 w-fit border border-green-900"
+          >
+            <lord-icon
+              src="https://cdn.lordicon.com/jgnvfzqg.json"
+              trigger="hover"
+            ></lord-icon>
+            Add Password
+          </button>
+        </div>
+        <div className="passwords">
+          <h2 className="font-bold text-2xl py-4">Your Passsword</h2>
+          {passwordArray.length === 0 && <div> No password to show</div>}
+          {passwordArray.length != 0 &&<table className="table-auto w-full rounded-md overflow-hidden">
+            <thead className=" bg-green-800 text-white">
+              <tr>
+                <th className="py-2">Site</th>
+                <th className="py-2">Username</th>
+                <th className="py-2">Password</th>
+              </tr>
+            </thead>
+            <tbody className="bg-green-100">
+              {passwordArray.map((item, index)=>{
+              return <tr key={index}>
+                <td className="py-2 border border-white text-center w-32 "><a href={item.site} target="_blank">{item.site}</a></td>
+                <td className="py-2 border border-white text-center w-32 ">{item.username}</td>
+                <td className="py-2 border border-white text-center w-32 ">{item.password}</td>
+              </tr>
+              })}
+              
+            </tbody>
+          </table>}
         </div>
       </div>
     </>
@@ -82,9 +129,6 @@ const Manager = () => {
 };
 
 export default Manager;
-
-
-
 
 // import React, { useState } from "react";
 // import { IoEye, IoEyeOffSharp } from "react-icons/io5";
